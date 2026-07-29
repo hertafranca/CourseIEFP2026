f1f831193ff92cbf9321dd0e1c92b676c6d72e46
Herta Franca <59263959+hertafranca@users.noreply.github.com>
2026-07-29 09:36:06 +0100

    atualização das aulas

---
+++ Phyton -Frederico Calor/SixthClass/Execise1.python.py
@@ -0,0 +1,11 @@
+
+#Criar um progama que de acordo com o número que foi introduzido pelo o usuário,
+# mostre a sua  sequência e ao mesmo tempo esses números estejam sendo somados:
+
+number=int(input("Digite um número:"))
+counter=1
+add=0
+
+while count<=number:
+   print(counter,end="")
+    
